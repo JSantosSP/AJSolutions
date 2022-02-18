f2ea53532b48398bb105db0be1dd72ebc52e6c3c
JSantosSP <jsantospalomera@gmail.com>
2022-02-18 19:09:17 +0100

    previo

--- ajsolutions/src/App.vue
+++ ajsolutions/src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <HeaderInit/>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HeaderInit from './components/HeaderInit.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, HeaderInit
   }
 }
 </script>
@@ -21,6 +22,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ ajsolutions/src/components/HeaderInit.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="header">
+    <div class="div1">
+      <div id="logo">
+        <img alt="logo" src="">
+      </div>
+      <div id="buscador">
+        <input type="text" name="navegador">
+      </div>
+      <div class="option">
+        <label>op1</label>
+      </div>
+      <div class="option">
+        <label>op2</label>
+      </div>
+      <div class="option">
+        <label>op3</label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeaderInit'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#header{
+  background: black;
+  margin: 0%;
+  color: white;
+}
+#buscador{
+  margin: 20px;
+}
+#logo{
+  margin-left: 0%;
+}
+.div1{
+  display: flex;
+  justify-content: flex-start;
+}
+.option{
+  margin: 20px;
+
+}
+
+</style>
